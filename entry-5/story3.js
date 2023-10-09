4ac9938f9cbd9cadc0471ba196154248ee2a3887
wcttzz <wangt165@newschool.edu>
2023-10-09 16:28:26 -0400

    entry-5
    new

---
+++ entry-5/story3.js
@@ -0,0 +1,28 @@
+const canvas=document.querySelector('canvas');
+const ctx=canvas.getContext('2d');
+
+const p={
+    x:0,
+    y:0,
+    r:50
+}
+
+function render(){
+    canvas.width=document.documentElement.clientWidth;
+    canvas.height=document.documentElement.clientHeight;
+    ctx.beginPath();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    var radial=ctx.createRadialGradient(p.x,p.y,p.r,p.x,p.y,p.r*5);
+    radial.addColorStop(0,'rgba(255,255,255,0)');
+    radial.addColorStop(1,'rgba(0,0,0,0.75)');
+    ctx.fillStyle=radial;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+document.onmousemove=e=>{
+    p.x=e.clientX;
+    p.y=e.clientY;
+    render();
+}
+
+render();
